465ed593bc4a29e0422ecb2a9cc7afe58a55d422
horrormyth <horrormyth@gmail.com>
2015-11-09 21:00:54 +0200

    Latest coin_contour

---
+++ mac_Camera_Capture.py
@@ -0,0 +1,12 @@
+__author__ = 'devndraghimire'
+
+import cv
+
+CAMERA_INDEX = 0
+
+cv.NamedWindow("Video", cv.CV_WINDOW_AUTOSIZE)
+capture = cv.CaptureFromCAM(CAMERA_INDEX)
+
+while True:
+    frame = cv.QueryFrame(capture)
+    cv.ShowImage("w1", frame)
